b82a5737ef7b30aca70fa3c08c12f6b2688c8b4c
Valentina Mota <122526853+valentina-mota@users.noreply.github.com>
2025-07-30 19:46:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,136 @@
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== ADD BLUR TO HEADER ====================*/
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the blur-header class
+    this.scrollY >= 50 ? header.classList.add("blur-header")
+                       : header.classList.remove("blur-header")
+}
+window.addEventListener('scroll', blurHeader)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+// const scrollHeader= ()=> {
+//     const header = document.getElementById('header')
+//     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+//     this.scrollY >= 50 ? header.classList.add("scroll-header")
+//                        : header.classList.remove("scroll-header")
+// }
+// window.addEventListener('scroll', scrollHeader)
+
+/*==================== SWIPER DISCOVER ====================*/
+
+/*==================== VIDEO ====================*/
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+window.addEventListener('scroll', () => {
+    const scrollUpBtn = document.getElementById('scroll-up');
+    console.log("scrollY:", window.scrollY);
+
+    if (window.scrollY >= 100) {
+        console.log("Mostro scroll-up");
+        scrollUpBtn.classList.add('show-scroll');
+    } else {
+        console.log("Nascondo scroll-up");
+        scrollUpBtn.classList.remove('show-scroll');
+    }
+});
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: '400',
+    //reset: true // Animation repeat
+})
+
+
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`)
+sr.reveal(`.home__card`, {delay: 600, distance: '100px', interval: 100})
+sr.reveal(`.about__data, .join__image`, {origin: 'right'})
+sr.reveal(`.about__image, .join__data`, {origin: 'left'})
+sr.reveal(`.popular__card`, {interval: 200})
+
+
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
